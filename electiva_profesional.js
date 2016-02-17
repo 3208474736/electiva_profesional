2c38346d866222e1b8c4a67979ed3ce4a8d2f089
3208474736 <villalobosernesto80@gmail.com>
2016-02-16 19:10:19 -0500

    codigo base

---
+++ electiva_profesional.js
@@ -0,0 +1,31 @@
+/*
+Dimas Ernesto González Villalobos
+Código: 561212122
+Electiva profesional 
+Profesor: Jorge Rubiano
+
+Código bases
+//   El Área de un es triángulo
+*/
+  var a, b, c;
+  b = prompt ("Ingrese número de la base");
+  h = prompt ("Ingresa número de la altura");
+  a = (b * h)/2;
+  console.log("El área de un triángulo es: " + a);
+
+//  El Área de un  círculo es
+
+  var r,a;
+  var pi= Math.PI;
+
+  r = prompt ("Ingresa el número del  radio");
+  a = (r * r * pi );
+  console.log("El área de un  círculo es: " + a);
+
+  // Área de un cuadrado es
+
+  var a, b, h;
+  b = prompt ("Ingresa número de la base");
+  h = prompt ("Ingresa número de la altura");
+  a = (b * h);
+  console.log("El área de un  cuadrado es: " + a);
